938e4cdd767c0d9facc564ce88d433afdc389466
Adam S <Jetbo@users.noreply.github.com>
2024-07-18 09:59:04 -0700

    replace example app with base expo 50 app

--- example/.eslintrc.js
+++ example/.eslintrc.js
@@ -1,4 +1,33 @@
+const isCI = process.env.CI === 'true'
+
 module.exports = {
-  root: true,
-  extends: '@react-native',
-};
+  extends: ['universe', 'universe/shared/typescript-analysis'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: ['n', 'jest', '@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-floating-promises': ['error', { ignoreIIFE: true }],
+    'jest/no-focused-tests': isCI ? 'error' : 'off',
+    'n/no-process-env': 'error',
+    'no-unused-expressions': [2, { allowTernary: true }],
+    'no-void': 'off',
+    semi: [2, 'never'],
+  },
+  // ignorePatterns: ['plugins/**/*.js'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx', '*.d.ts'],
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    {
+      files: ['__tests__/**/*', '__mocks__/**/*'],
+      rules: {
+        'n/no-process-env': 'off',
+      },
+    },
+  ],
+}
---
+++ example/app/_layout.tsx
@@ -0,0 +1,27 @@
+// Import your global CSS file
+import '../global.css'
+
+import React from 'react'
+import { PaperProvider } from 'react-native-paper'
+import BleManager from 'react-native-ble-manager'
+
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from 'expo-router'
+
+// Only runs once per app load
+// https://react.dev/learn/you-might-not-need-an-effect#initializing-the-application
+if (typeof window !== 'undefined') {
+  try {
+    BleManager.start({ showAlert: false }).then(() => console.debug('BleManager started'))
+  } catch (error) {
+    console.error('unexpected error starting BleManager', error)
+  }
+}
+
+export default function RootLayout() {
+  return (
+    <PaperProvider></PaperProvider>
+  )
+}
